c83aaf009ee4cc9b30fe3f61c76b66f747d3446d
rahul <rahul.adsps@gmail.com>
2020-12-23 22:33:24 -0800

    adding files

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        try {
+            $credentials = request(['email', 'password']);
+            if (Auth::attempt($credentials)) {
+                $user = User::where('email', $request->email)->first();
+                $token = $user->createToken($request->device_name)->plainTextToken;
+                return $this->jsonResponse('Login Success', ['token' => $token, 'user' => $user]);
+            }
+        } catch (\Throwable $th) {
+            return $th;
+        }
+        return $this->jsonResponse('Login Failed', []);
+    }
+    public function user()
+    {
+        $user = Auth::User();
+        return $this->jsonResponse('Success', ['user' =>$user]);
+    }
+    public function logout()
+    {
+        $user =  User::find(Auth::User()->id);
+        try {
+            $user->tokens()->delete();
+        } catch (\Throwable $th) {
+            return $this->jsonResponse('Logout Failed', []);
+        }
+        return $this->jsonResponse('Logout Success', []);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', [UserController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'user']);
+});
+Route::post('/token', [UserController::class, 'login']);
